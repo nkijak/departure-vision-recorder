2515d660792465c86740a4b350315218c8f42891
Nicolas Kijak <nkijak@gmail.com>
2019-04-14 16:11:00 -0400

    openfaas: fixing sync-db path hardcoded based on openfaas image

--- couchdb/handler.py
+++ couchdb/handler.py
@@ -8,4 +8,4 @@ def handle(req, couch=None):
         host = os.environ.get('DB_HOST', '192.168.1.211')
         port = int(os.environ.get('DB_PORT', '5984'))
         couch = Repo(host, port) 
-    couch.connect()
+    couch.connect(design_root="/home/app/function/couchdb") #TODO figure out how to do this correctly with relative roots later
